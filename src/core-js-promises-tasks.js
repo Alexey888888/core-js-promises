571f9b161336e5d5e85a3da7b3e9389e13132cc4
Alexey888888 <brdkmail@gmail.com>
2024-03-20 16:41:33 +0200

    feat: implement queuPromises function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -150,8 +150,15 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let resPromise = '';
+  return promises
+    .reduce(async (res, currentPromise) => {
+      await res;
+      const value = await currentPromise;
+      resPromise += value;
+    }, Promise.resolve())
+    .then(() => resPromise);
 }
 
 module.exports = {
